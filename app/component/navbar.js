300fe3ae6b509ea35220b1e45835df797d897875
MKHLink <likhonhasankomol@gmail.com>
2025-01-22 22:37:43 -0500

    added navbar, hero section and message

---
+++ app/component/navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NavBar(){
+    return(
+            <header className='navBar'>
+                <div className='comName'>Find Your Pet</div>
+                <div>Who We Are</div>
+                <div>Why Adopt</div>
+                <div>Contact Us</div>
+            </header>
+    );
+}
